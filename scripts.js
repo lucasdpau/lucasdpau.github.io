b6d9b3f3308b2912cea70c2bf0a2a02f7d0a49b3
lp <lucasdpau@gmail.com>
2020-05-21 15:56:39 -0400

    finished javascript to open/close skills tabs

--- scripts.js
+++ scripts.js
@@ -7,25 +7,28 @@ const buttonLang = document.getElementById("language_minimize");
 const tech = document.getElementById("techs-wrapper");
 const buttonTech = document.getElementById("techs_minimize");
 
-buttonTech.onclick = function() {
-	if (techToggle == 0) { 
-		tech.classList.add("invisible");
-		techToggle = 1;
-		}
-	else {
-		tech.classList.remove("invisible");
-		techToggle = 0;	
-		}
-	};
-
 buttonLang.onclick = function() {
 	if (langToggle == 0) { 
+		buttonLang.innerHTML = '[+]'
 		lang.classList.add("invisible");
 		langToggle = 1;
 		}
 	else {
+		buttonLang.innerHTML = '[-]'
 		lang.classList.remove("invisible");
 		langToggle = 0;	
 		}
 	};
 
+buttonTech.onclick = function() {
+	if (techToggle == 0) { 
+		buttonTech.innerHTML = '[+]'
+		tech.classList.add("invisible");
+		techToggle = 1;
+		}
+	else {
+		buttonTech.innerHTML = '[-]'
+		tech.classList.remove("invisible");
+		techToggle = 0;	
+		}
+	};
